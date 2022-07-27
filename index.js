efffa6cd09ab0854909f4fa3ec002573a75ee026
Wolke <wolkesau@gmail.com>
2022-07-27 23:12:01 +0800

    1.0.5

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-var http = require("http");
-const TaiwanWeather = require(`./TaiwanWeather.js`);
+module.exports = require(`./TaiwanWeather.js`);
+// var http = require("http");
+// const TaiwanWeather = require(`./TaiwanWeather.js`);
 // (async () => {
 //   console.log(`hello`);
 //   let data = await TaiwanWeather(process.env.key, `新北市`);
 //   console.log(data);
 // })();
 // //create a server object:
-http
-  .createServer(function (req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+// http
+//   .createServer(function (req, res) {
+//     res.write("Hello World!"); //write a response to the client
+//     res.end(); //end the response
+//   })
+//   .listen(8080); //the server object listens on port 8080
